7f186d3f97a0262c69676131fd69694054c35a22
Michel Grolet <39418855+MichelGrolet@users.noreply.github.com>
2023-12-18 13:13:53 +0100

    init tests

---
+++ spotify.js
@@ -0,0 +1,44 @@
+async function fetchSpotifyPlaylists(token) {
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        return response.data.items.map(playlist => ({
+            name: playlist.name,
+            url: playlist.external_urls.spotify
+        }));
+    } catch (error) {
+        console.error('Error fetching Spotify playlists:', error);
+        return [];
+    }
+}
+
+function displayPlaylists(playlists) {
+    const container = document.getElementById('playlistContainer');
+    playlists.forEach(playlist => {
+        const item = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = playlist.url;
+        link.textContent = playlist.name;
+        link.target = '_blank';
+
+        item.appendChild(link);
+        container.appendChild(item);
+    });
+}
+
+(async function() {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const accessToken = params.get('access_token');
+
+    if (accessToken) {
+        const playlists = await fetchSpotifyPlaylists(accessToken);
+        displayPlaylists(playlists);
+    } else {
+        console.error('Access token not found');
+    }
+})();
